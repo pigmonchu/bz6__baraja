1ca7fe008a72a4ada55d1543353f749c57be0e25
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-11-12 00:37:18 +0100

    Fin creación clase Baraja

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import baraja
+import barajaC
 
 palos = ['corazones', 'picas', 'diamantes', 'treboles']
 numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-ordenada = baraja.creaBaraja(palos, numeros)
-print("Esta es la primera baraja: ",ordenada)
+miBaraja = barajaC.Baraja(palos, numeros)
 
-otraBaraja =  baraja.creaBaraja(palos, numeros)
-print("Esta es la segunda baraja nada más crearla:", otraBaraja)
-baraja.barajar(otraBaraja)
-print("Y ahora la he barajado:", otraBaraja)
-print("Para que fernando se lo crea, la baraja primera:", ordenada)
+print(miBaraja.mazacote)
+miBaraja.barajar()
+print(miBaraja.mazacote)
